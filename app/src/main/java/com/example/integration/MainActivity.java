c6a2511dccaa1ae73ce892eb03eab59a278ffa7a
Anastasia Kalugina <anastasiakalugina@MacBook-Air-Anastasia.local>
2020-09-09 15:59:29 +0300

    second

--- app/src/main/java/com/example/integration/MainActivity.java
+++ app/src/main/java/com/example/integration/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.integration;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.multidex.MultiDex;
+
+import com.appodeal.ads.Appodeal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Appodeal.initialize(this, "590a524b2c1842b81da4fa868f8c229bee170199e8e95428", adTypes, consentValue);
+        Appodeal.initialize(this, "590a524b2c1842b81da4fa868f8c229bee170199e8e95428", Appodeal.BANNER,true);
     }
 }
