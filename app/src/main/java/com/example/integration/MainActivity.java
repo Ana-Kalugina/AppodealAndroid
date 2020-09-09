fd01be8eabfdab4b69d7c4b3c38549a88940848e
Anastasia Kalugina <anastasiakalugina@MacBook-Air-Anastasia.local>
2020-09-09 17:31:48 +0300

    third

--- app/src/main/java/com/example/integration/MainActivity.java
+++ app/src/main/java/com/example/integration/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.integration;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.multidex.MultiDex;
@@ -9,11 +12,22 @@ import androidx.multidex.MultiDex;
 import com.appodeal.ads.Appodeal;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Appodeal.initialize(this, "590a524b2c1842b81da4fa868f8c229bee170199e8e95428", Appodeal.BANNER,true);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity2();
+            }
+        });
+    }
+    public void openMainActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
