1e65156dbee73cd77c0712223b82668384711ec4
Anastasia Kalugina <anastasiakalugina@MacBook-Air-Anastasia.local>
2020-09-09 23:56:29 +0300

    five

--- app/src/main/java/com/example/integration/MainActivity.java
+++ app/src/main/java/com/example/integration/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.integration;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,19 +11,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.multidex.MultiDex;
 
 import com.appodeal.ads.Appodeal;
+import com.appodeal.ads.BannerCallbacks;
+import com.appodeal.ads.NativeAdView;
 import com.appodeal.ads.RewardedVideoCallbacks;
 import com.appodeal.ads.api.App;
+import com.appodeal.ads.utils.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private Button button2;
     private Button button3;
     private Button button4;
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Appodeal.initialize(this, "590a524b2c1842b81da4fa868f8c229bee170199e8e95428", Appodeal.BANNER,true);
+        Appodeal.setLogLevel(Log.LogLevel.verbose);
+        Appodeal.initialize(this, "590a524b2c1842b81da4fa868f8c229bee170199e8e95428", Appodeal.BANNER | Appodeal.INTERSTITIAL
+                | Appodeal.REWARDED_VIDEO | Appodeal.NATIVE,true);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,58 +65,62 @@ public class MainActivity extends AppCompatActivity {
     }
     public void showBanner() {
         Appodeal.show(this, Appodeal.BANNER_TOP);
-//        Intent intent = new Intent(this, MainActivity2.class);
-//        startActivity(intent);
+    Appodeal.setBannerCallbacks(new BannerCallbacks() {
+        @Override
+        public void onBannerLoaded(int i, boolean b) {
+
+        }
+
+        @Override
+        public void onBannerFailedToLoad() {
+
+        }
+
+        @Override
+        public void onBannerShown() {
+            count ++;
+        if (count >=5 ){
+            Appodeal.hide(MainActivity.this,Appodeal.BANNER_TOP);
+            }
+        }
+
+        @Override
+        public void onBannerShowFailed() {
+
+        }
+
+        @Override
+        public void onBannerClicked() {
+
+        }
+
+        @Override
+        public void onBannerExpired() {
+
+        }
+    });
     }
-    public void showInterstitial() {
-        Appodeal.show(this,Appodeal.INTERSTITIAL);
-        Appodeal.isLoaded(Appodeal.INTERSTITIAL);
-        Appodeal.setAutoCache(Appodeal.INTERSTITIAL, false);
-        Appodeal.cache(this, Appodeal.INTERSTITIAL);
+    public void showInterstitial(){
+        Appodeal.hide(this,Appodeal.BANNER_TOP);
+        if(Appodeal.isLoaded(Appodeal.INTERSTITIAL)) {
+            Appodeal.show(this, Appodeal.INTERSTITIAL);
+            
+        }
     }
     public void showRewardedVideo() {
-        Appodeal.show(this,Appodeal.REWARDED_VIDEO);
-        Appodeal.isLoaded(Appodeal.REWARDED_VIDEO);
-        Appodeal.setRewardedVideoCallbacks(new RewardedVideoCallbacks() {
-            @Override
-            public void onRewardedVideoLoaded(boolean isPrecache) {
-                // Called when rewarded video is loaded
-            }
-            @Override
-            public void onRewardedVideoFailedToLoad() {
-                // Called when rewarded video failed to load
-            }
-            @Override
-            public void onRewardedVideoShown() {
-                // Called when rewarded video is shown
-            }
-            @Override
-            public void onRewardedVideoShowFailed() {
-                // Called when rewarded video show failed
-            }
-            @Override
-            public void onRewardedVideoClicked() {
-                // Called when rewarded video is clicked
-            }
-            @Override
-            public void onRewardedVideoFinished(double amount, String name) {
-                // Called when rewarded video is viewed until the end
-            }
-            @Override
-            public void onRewardedVideoClosed(boolean finished) {
-                // Called when rewarded video is closed
-            }
-            @Override
-            public void onRewardedVideoExpired() {
-                // Called when rewarded video is expired
-            }
-
-        });
-        Appodeal.setAutoCache(Appodeal.REWARDED_VIDEO, false);
-        Appodeal.cache(this, Appodeal.REWARDED_VIDEO);
+        Appodeal.hide(this,Appodeal.BANNER_TOP);
+        if(Appodeal.isLoaded(Appodeal.REWARDED_VIDEO)) {
+            Appodeal.show(this,Appodeal.REWARDED_VIDEO);
+        }
     }
 
     public void showNative() {
-
+        Appodeal.hide(this,Appodeal.BANNER_TOP);
+        Appodeal.cache(this, Appodeal.NATIVE, 4);
+        Appodeal.isLoaded(Appodeal.NATIVE);
+        //Appodeal.getNativeAds(int ammount);
+        Appodeal.show(this,Appodeal.NATIVE);
     }
+
+
 }
